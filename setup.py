a4730b66cdc09c29a075906bbcc83ad4502b5fb8
kunyink <santo.lpu@gmail.com>
2021-10-23 12:48:29 +0700

    beta

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ setup(
     description='Lisensi aplikasi menggunakan realtime database dari firebase',
     author='santo',
     author_email='santo.lpu@gmail.com',
+    url='',
     license='MIT',
-    install_requires=['pycryptodome','requests'],
+    install_requires=['pycryptodome', 'requests'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     test_suite='test',
 )
-
